48b506feb0ec6e4847a807a2ec820d9b8db42ab5
tamaki motofumi <tamakimotofumi@gmail.com>
2026-08-11 22:02:40 +0900

    sidebarの月次勤怠の新規追加ボタンを非表示

--- backoffice/settings.py
+++ backoffice/settings.py
@@ -36,14 +36,14 @@ ALLOWED_HOSTS = []
 
 INSTALLED_APPS = [
     "import_export",
+    "common",
+    "kintai",
     "django.contrib.admin",
     "django.contrib.auth",
     "django.contrib.contenttypes",
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "common",
-    "kintai",
 ]
 
 MIDDLEWARE = [
